965e22c470c53c890e67776a0a12a4b467b984fb
w4rex <eiv13281@soisz.com>
2014-11-09 23:43:13 +0200

    Update AssemblyInfo.cs

--- Annie/Properties/AssemblyInfo.cs
+++ Annie/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Annie")]
+[assembly: AssemblyTitle("Annie Anan")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Annie")]
+[assembly: AssemblyProduct("Annie Anan")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
