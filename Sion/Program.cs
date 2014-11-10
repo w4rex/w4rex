c56477e66a38c0a45884fe99d46ba00d7a760bf0
w4rex <eiv13281@soisz.com>
2014-11-10 17:04:44 +0200

    Update Program.cs

--- Sion/Program.cs
+++ Sion/Program.cs
@@ -40,25 +40,80 @@ namespace Sion
             Config = new Menu("t3h 3xpl01t", "t3h 3xpl01t", true);
             
             Config.AddSubMenu(new Menu("3xpl01t", "3xpl01t"));
-            Config.SubMenu("3xpl01t").AddItem(new MenuItem("AntiCamLock", "exploiti aç").SetValue(true));
+            Config.SubMenu("3xpl01t").AddItem(new MenuItem("AntiCamLock", "Avoid locking camera").SetValue(new KeyBind("L".ToCharArray()[0] KeyBindType.Toggle)));
 
             Config.AddToMainMenu();
 
-            Game.PrintChat("t3h 3xplo1t l04d3d - w4rex - yani ozan - yani uğur ozan - yani puro - yani reyiz!");
+            Game.PrintChat("ben iflah olmaz bir seks makinesiyim - tufan - ozan - onur");
             Game.OnGameUpdate += Game_OnGameUpdate;
             Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+            Drawing.OnDraw += Drawing_OnDraw;
         }
 
-        static void Game_OnGameProcessPacket(GamePacketEventArgs args)
+        static void Game_OnGameUpdate(EventArgs args)
         {
-            if (args.PacketData[0] == 0xFE && Config.Item("AntiCamLock").GetValue<bool>())
+            //Casting R
+            if (ObjectManager.Player.HasBuff("SionR"))
             {
-                var p = new GamePacket(args.PacketData);
-                if (p.ReadInteger(1) == ObjectManager.Player.NetworkId && p.Size() > 9)
+                if (Config.Item("MoveToMouse").GetValue<bool>())
                 {
-                    args.Process = false;
+                    var p = ObjectManager.Player.Position.To2D().Extend(Game.CursorPos.To2D(), 500);
+                    ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, p.To3D());
+                }
+                return;
+            }
+
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                var qTarget = SimpleTs.GetTarget(!Q.IsCharging ? Q.ChargedMaxRange / 2 : Q.ChargedMaxRange, SimpleTs.DamageType.Physical);
+
+                var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+
+                if (qTarget != null && Config.Item("UseQCombo").GetValue<bool>())
+                {
+                    if (Q.IsCharging)
+                    {
+                        var start = ObjectManager.Player.ServerPosition.To2D();
+                        var end = start.Extend(QCastPos, Q.Range);
+                        var direction = (end - start).Normalized();
+                        var normal = direction.Perpendicular();
+
+                        var points = new List<Vector2>();
+                        var hitBox = qTarget.BoundingRadius;
+                        points.Add(start + normal * (Q.Width + hitBox));
+                        points.Add(start - normal * (Q.Width + hitBox));
+                        points.Add(end + Q.ChargedMaxRange * direction - normal * (Q.Width + hitBox));
+                        points.Add(end + Q.ChargedMaxRange * direction + normal * (Q.Width + hitBox));
+
+                        for (int i = 0; i <= points.Count - 1; i++)
+                        {
+                            var A = points[i];
+                            var B = points[i == points.Count - 1 ? 0 : i + 1];
+
+                            if (qTarget.ServerPosition.To2D().Distance(A, B, true, true) < 50 * 50)
+                            {
+                                Packet.C2S.ChargedCast.Encoded(new Packet.C2S.ChargedCast.Struct((SpellSlot)((byte)Q.Slot), Game.CursorPos.X, Game.CursorPos.X, Game.CursorPos.X)).Send();
+                            }
+                        }
+                        return;
+                    }
+                    
+                    if(Q.IsReady())
+                    {
+                        Q.StartCharging(qTarget.ServerPosition);
+                    }
+                }
+
+                if (qTarget != null && Config.Item("UseWCombo").GetValue<bool>())
+                {
+                    ObjectManager.Player.Spellbook.CastSpell(SpellSlot.W, ObjectManager.Player);
+                }
+
+                if (eTarget != null && Config.Item("UseECombo").GetValue<bool>())
+                {
+                    E.Cast(eTarget);
                 }
             }
         }
-            }
-        }
+    }
+}
